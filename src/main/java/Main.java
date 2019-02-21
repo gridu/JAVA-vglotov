c7598f442ef0a4fc94ca678974ce5cfc909f501c
Vladimir Glotov <glotovv@ae.com>
2019-02-21 22:35:08 +0400

    sample external sorting added correction for not repeated number of elements to halMaxRam value

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,16 @@
-
-import com.sun.scenario.effect.Merge;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         List<String> ramArray1;
         List<String> ramArray2;
-        String[] initial = {"glotov", "vladimir", "gonna", "solve", "external", "sorting", "problem", "car", "bed", "food", "sleep", "atnight"};
-        int maxRam = 6;
+        String[] initial = {"glotov", "vladimir", "gonna", "solve", "external", "sorting", "problem", "car", "baed", "afood", "sleep"};
+        int maxRam = 5;
         int halfMaxRam = maxRam/2;
         int maxCycle = initial.length/halfMaxRam;
         if (initial.length%halfMaxRam > 0) {
-            maxCycle =+ 1;
+            maxCycle += 1;
         }
 
         System.out.println("Initial array at the beginning:");
@@ -26,19 +20,32 @@ public class Main {
 
         for (int p = 0; p < maxCycle-1; p++) {
             for (int j = 0; j < maxCycle - 1 -p; j++) {
-                System.out.println("\n\nMacrocycle number " + (p + 1));
-                System.out.println("Microcycle number " + (j + 1));
+                System.out.println("\n\n--Macrocycle number " + (p + 1) + "--");
+                System.out.println("--Microcycle number " + (j + 1) + "--");
                 String[] subArray1 = new String[halfMaxRam];
                 String[] subArray2 = new String[halfMaxRam];
+                boolean isCut = false;
+                int cutLength = 0;
 
                 System.arraycopy(initial, p * halfMaxRam, subArray1, 0, halfMaxRam);
                 for (int i = 0; i < halfMaxRam; i++) {
-                    subArray2[i] = initial[(i+p*halfMaxRam) +halfMaxRam + halfMaxRam * j];
+                    if ((i+p*halfMaxRam) + halfMaxRam + halfMaxRam * j > initial.length-1){
+                        System.out.println("\nSecond array is cut!");
+                        isCut = true;
+                        String[] newArr = new String[i];
+                        System.arraycopy(subArray2, 0, newArr, 0, i);
+                        subArray2 = new String[i];
+                        subArray2 = newArr;
+                        cutLength = subArray2.length;
+                        break;
+                    }
+                    subArray2[i] = initial[(i+p*halfMaxRam) + halfMaxRam + halfMaxRam * j];
                 }
-                ramArray1 = new ArrayList<String>(Arrays.asList(subArray1));
-                ramArray2 = new ArrayList<String>(Arrays.asList(subArray2));
 
-                System.out.println("ramArr1:");
+                ramArray1 = new ArrayList<>(Arrays.asList(subArray1));
+                ramArray2 = new ArrayList<>(Arrays.asList(subArray2));
+
+                System.out.println("\nramArr1:");
                 for (String number : ramArray1) {
                     System.out.print(number + " ");
                 }
@@ -59,8 +66,14 @@ public class Main {
                 }
                 System.out.println();
 
-                for (int i = 0; i < halfMaxRam; i++) {
-                    ramArray2.add(i, ramArray1.get(i + halfMaxRam));
+                if (isCut) {
+                    for (int i = 0; i < cutLength; i++) {
+                        ramArray2.add(i, ramArray1.get(i + halfMaxRam));
+                    }
+                } else {
+                    for (int i = 0; i < halfMaxRam; i++) {
+                        ramArray2.add(i, ramArray1.get(i + halfMaxRam));
+                    }
                 }
 
                 System.out.println("ramArr2 after refactoring:");
@@ -73,13 +86,22 @@ public class Main {
                     initial[(i+p*halfMaxRam)] = ramArray1.get(i);
                 }
 
-                for (int i = 0; i < halfMaxRam; i++) {
-                    initial[(i+p*halfMaxRam) + halfMaxRam + halfMaxRam * j] = ramArray1.get(i + halfMaxRam);
+                if (!isCut) {
+                    for (int i = 0; i < halfMaxRam; i++) {
+                        initial[(i + p * halfMaxRam) + halfMaxRam + halfMaxRam * j] = ramArray1.get(i + halfMaxRam);
+                    }
+                } else {
+                    for (int i = 0; i < cutLength; i++) {
+                        initial[(i + p * halfMaxRam) + halfMaxRam + halfMaxRam * j] = ramArray1.get(i + halfMaxRam);
+                    }
                 }
+
                 System.out.println("New sorted big array after cycle:");
                 for (String str : initial) {
                     System.out.print(str + ' ');
                 }
+//                isCut = false;
+//                cutLength = 0;
             }
         }
     }
