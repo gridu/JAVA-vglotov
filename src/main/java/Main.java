124398d37fd78b4f5248b2fdacc02bd6be9d615d
Glotov Vladimir <glotovv@ae.com>
2019-05-27 23:38:43 +0400

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-import org.testng.annotations.Test;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -20,8 +18,8 @@ public class Main {
 
     public static int getMinStrCount(String path, int maxRam, long lineCount) throws IOException {
         int biggestLineSize = 0;
-        int tempSize = 0;
         int minStringCount = 0;
+        int tempSize;
 
         LineNumberReader lnr = new LineNumberReader(new FileReader(path));
 
@@ -45,7 +43,6 @@ public class Main {
 
     public static void sortFile(String path, int maxRam) throws IOException {
 
-//        LineNumberReader lnr = new LineNumberReader(new FileReader(path));
         RandomAccessFile file = new RandomAccessFile(new File(path), "rw");
         long totalLines = Files.lines(Paths.get(path)).count();
         int minStringCount = getMinStrCount(path, maxRam, totalLines);
@@ -86,29 +83,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //потом использовать writeChars не в этом методе
-        //может придется удалять строки
-    }
-
-
-    private static long fillArrayList(RandomAccessFile file, long startPos, List<String> destArray, long halfMaxRam) throws IOException {
-        long tempMemory = 0;
-        file.seek(startPos);
-        String element;
-
-        while (tempMemory < halfMaxRam) {
-            long returnToPos = file.getFilePointer();
-            element = file.readLine();
-            if (element == null || sum(tempMemory, element.length() * 2) > halfMaxRam) {
-                file.seek(returnToPos);
-                break;
-            } else {
-                destArray.add(element);
-                tempMemory = sum(tempMemory, element.length() * 2);
-            }
-        }
-        return file.getFilePointer();
     }
 
     private static void writeToFile(RandomAccessFile file, long startPos, List<String> srcArray) throws IOException {
@@ -117,86 +91,5 @@ public class Main {
         for (String str : srcArray) {
             file.writeBytes(str + "\n");
         }
-
     }
-
-//    public static void sort(String[] initial, int maxRam) {
-//        List<String> ramArray1;
-//        List<String> ramArray2;
-//        int halfMaxRam = maxRam / 2;
-//        int maxCycle = initial.length / halfMaxRam;
-//        if (initial.length % halfMaxRam > 0) {
-//            maxCycle += 1;
-//        }
-//
-//        for (int p = 0; p < maxCycle - 1; p++) {
-//            for (int j = 0; j < maxCycle - 1 - p; j++) {
-//                String[] subArray1 = new String[halfMaxRam];
-//                String[] subArray2 = new String[halfMaxRam];
-//                boolean isCut = false;
-//                int cutLength = 0;
-//
-//                System.arraycopy(initial, p * halfMaxRam, subArray1, 0, halfMaxRam);
-//                for (int i = 0; i < halfMaxRam; i++) {
-//                    if ((i + p * halfMaxRam) + halfMaxRam + halfMaxRam * j > initial.length - 1) {
-//                        isCut = true;
-//                        String[] newArr = new String[i];
-//                        System.arraycopy(subArray2, 0, newArr, 0, i);
-//                        subArray2 = new String[i];
-//                        subArray2 = newArr;
-//                        cutLength = subArray2.length;
-//                        break;
-//                    }
-//                    subArray2[i] = initial[(i + p * halfMaxRam) + halfMaxRam + halfMaxRam * j];
-//                }
-//
-//                ramArray1 = new ArrayList<>(Arrays.asList(subArray1));
-//                ramArray2 = new ArrayList<>(Arrays.asList(subArray2));
-//
-//                ramArray1.addAll(ramArray2);
-//                ramArray2.clear();
-//                Collections.sort(ramArray1);
-//
-//                if (isCut) {
-//                    for (int i = 0; i < cutLength; i++) {
-//                        ramArray2.add(i, ramArray1.get(i + halfMaxRam));
-//                    }
-//                } else {
-//                    for (int i = 0; i < halfMaxRam; i++) {
-//                        ramArray2.add(i, ramArray1.get(i + halfMaxRam));
-//                    }
-//                }
-//
-//                for (int i = 0; i < halfMaxRam; i++) {
-//                    initial[(i + p * halfMaxRam)] = ramArray1.get(i);
-//                }
-//
-//                if (!isCut) {
-//                    for (int i = 0; i < halfMaxRam; i++) {
-//                        initial[(i + p * halfMaxRam) + halfMaxRam + halfMaxRam * j] = ramArray1.get(i + halfMaxRam);
-//                    }
-//                } else {
-//                    for (int i = 0; i < cutLength; i++) {
-//                        initial[(i + p * halfMaxRam) + halfMaxRam + halfMaxRam * j] = ramArray1.get(i + halfMaxRam);
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-//    private static void printArray(String[] array) {
-//        System.out.println("\nArray values:");
-//        for (String str : array) {
-//            System.out.print(str + ' ');
-//        }
-//    }
-//
-//    @Test
-//    private static void sortTest() {
-//        String[] actual = {"a", "c", "e", "3", "f", "d", "b"};
-//        String[] expected = {"3", "a", "b", "c", "d", "e", "f"};
-//        sort(actual, 3);
-//        printArray(actual);
-//        assert (Arrays.equals(actual, expected));
-//    }
 }
